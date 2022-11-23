279ebf46a9f597a397ec784fa06157f11d4fb1c8
sahad sathar <sahadsathar1997@gmail.com>
2022-11-23 07:41:53 +0500

    CRUD and modles creation

--- todolist/views.py
+++ todolist/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from . models import *
+from rest_framework import generics
 
 # Create your views here.
+# CRUD operation
+class ListTodo(generics.ListAPIView):   # Read
+    queryset=Todo.objects.all()
+
+class DetailTodo(generics.RetrieveUpdateAPIView): # Update
+    queryset = Todo.objects.all()
+
+class CreateTodo(generics.CreateAPIView):  # Create
+    queryset = Todo.objects.all()
+
+class DeleteTodo(generics.DestroyAPIView): # Delete
+    queryset = Todo.objects.all()
+
+
 def index(request):
     return render(request, 'index.html')
